46909cbf7267e4fa1ff64ee4d903c14dad2d5b17
Kumkum Singh <ks3022323@gmail.com>
2026-08-25 20:11:35 +0530

    Added Codeforces 510A Fox And Snake solution

---
+++ 510A_Fox_And_Snake/Main.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                // Full row
+                System.out.println("#".repeat(m));
+            } else {
+                // Alternating rows
+                if (i % 4 == 1) {
+                    System.out.println(".".repeat(m - 1) + "#");
+                } else {
+                    System.out.println("#" + ".".repeat(m - 1));
+                }
+            }
+        }
+
+        sc.close();
+    }
+}
--- README.md
+++ README.md
@@ -57,6 +57,7 @@ This repository contains my solutions to Codeforces problems solved in Java.
 | 2250A | Threshold Movement | [Code](2250A_Threshold_Movement/Main.java) | ✅ Accepted |
 | 2252B | Always Changing | [Code](2252B_Always_Changing/Main.java) | ✅ Accepted |
 | 2252C | Risky Tower | [Code](2252C_Risky_Tower/Main.java) | ✅ Accepted |
+| 510A | Fox And Snake | [Code](510A_Fox_And_Snake/Main.java) | ✅ Accepted |
 
 
 
